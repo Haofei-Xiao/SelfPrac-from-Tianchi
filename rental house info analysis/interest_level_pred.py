07100980dfd61642f0c57577bf9a261d1d82b0bc
Haofei_Xiao <xhf2420819202@gmail.com>
2020-12-27 16:14:01 +1100

    Same data but prediction
    Using training data to predict the interest level of testing data. The only question is cannot calculate the accuracy score because I dont have the actual y.

---
+++ rental house info analysis/interest_level_pred.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder
+
+x_col = ["bathrooms", "bedrooms", "latitude", "longitude", "price"]
+
+df1= pd.read_json('train.json')
+xtrain = df1[x_col]
+
+d = {'low':1,'medium':2,'high':3}
+df1["interest_level"] = df1["interest_level"].map(d)
+ytrain = df1[["interest_level"]]
+
+
+df2 = pd.read_json('test.json')
+xtest = df2[x_col]
+
+
+model = DecisionTreeRegressor()
+model.fit(xtrain, ytrain)
+pred = model.predict(xtest)
+df2['pred_level'] = pred
+df2['pred_level'] = df2['pred_level'].astype(int)
+d2 = {1:'low',2:'medium', 3:'high'}
+df2['pred_level']  = df2['pred_level'] .map(d2)
+df1["interest_level"] = df1["interest_level"].map(d2)
+
+df1.to_csv("training_data.csv")
+df2.to_csv("testing_data.csv")
