7465e2bb9e09cb84d0ee1586de7e4eb55b4ebcd1
Haofei_Xiao <xhf2420819202@gmail.com>
2021-01-02 14:22:20 +1100

    Update boxplot.py

--- rental house info analysis (visual + ML + report)/data visualization/boxplot.py
+++ rental house info analysis (visual + ML + report)/data visualization/boxplot.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 #https://stackoverflow.com/questions/23232989/boxplot-stratified-by-column-in-python-pandas
 #https://stackoverflow.com/questions/38927459/pandas-boxplot-with-ranges-in-x-axis
 df = pd.read_json("train.json")
-
+#print(df['price'].describe())
 
 df['log'] = pd.qcut(df['longitude'],5)
 df['lat'] = pd.qcut(df['latitude'],5)
@@ -13,8 +13,8 @@ df['lat'] = pd.qcut(df['latitude'],5)
 fig, ax =plt.subplots(1,2, figsize=(18, 10))
 sns.boxplot(x=df["log"], y=df["price"], data=df, ax=ax[0])
 sns.boxplot(x=df["lat"], y=df["price"], data=df, ax=ax[1])
-ax[0].set_ylim(0,7000)
-ax[1].set_ylim(0,7000)
+ax[0].set_ylim(0,5000)
+ax[1].set_ylim(0,5000)
 
 plt.savefig("lat&logprice_boxplot.png")
 plt.show()
